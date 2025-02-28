c3021c24cfaf4e12c1611658bfa3381072a11cbb
Ashikur Zaman Kollol <zamankollol12@gmail.com>
2025-03-01 01:19:16 +0600

    main.c

--- main.c
+++ main.c
@@ -3,14 +3,14 @@ int main(){
 
 
 int arr[]={-99, 45, 100, 37, 89, -327, 245};
-  int max = arr[0],i;
+  int max = arr[0],i; //for zero index.
 
 for(i=1;i<7;i++){
 
     if(arr[i]>max)
-        max=arr[i];
+        max=arr[i]; //for update the value.
 }
-   printf("%d\n",max);
+   printf("%d\n",max); //print outside the of the loop
 
 
 return 0;
